b501dbb805dd23a23773c16026e5c173b8a2b427
Mayank08082004 <mayank1533@gmail.com>
2024-01-07 20:55:49 +0530

    Add

---
+++ src/com/mayank/Inputs.java
@@ -0,0 +1,11 @@
+package com.mayank;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float a = input.nextFloat();
+        System.out.println("Your num is "+a);
+    }
+}
---
+++ src/com/mayank/Typecasting.java
@@ -0,0 +1,16 @@
+package com.mayank;
+
+import java.util.Scanner;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int count = 1;
+        while(count != 5){
+            System.out.println(count);
+            count++;
+        }
+
+    }
+}
